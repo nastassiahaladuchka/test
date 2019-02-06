c484982d538d23d723f86769be1fd7a90d321c0e
Nastya <nastassiamiadzvedz@mail.ru>
2019-02-06 20:38:37 +0300

    New1

---
+++ лекции/условия/script.js
@@ -0,0 +1,9 @@
+let num = 50;
+if (num < 49)
+{
+	console.log ( "neverno" );
+}else if (num > 100) {
+	console.log ("mnogo");
+} else {
+	console.log ( "okay" );
+}
---
+++ лекции/циклы/script.js
@@ -0,0 +1,16 @@
+/*let num = 50;
+/*while (num < 55){
+	console.log (num);
+	num++;
+}
+do{
+	console.log (num);
+	num++;
+}
+while (num < 55);
+
+for (let i = 1; i<8; i++){
+	console.log (i);
+}
+let y = 1; let x = y = 2; alert(x);*/
+alert(0 || "" || 2 || undefined || true || falsе)
